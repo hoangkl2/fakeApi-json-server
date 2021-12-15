fae47713d07bad979e87fdea76e975a9996c0aa8
luukimhoang2k <luukimhoangdz2022@gmail.com>
2021-12-15 12:48:59 +0700

    fake api

---
+++ generate-data.js
@@ -0,0 +1,81 @@
+const faker = require('faker');
+
+const fs = require('fs');
+
+faker.locale= 'vi'
+//Random data
+// console.log(faker.commerce.department());
+// console.log(faker.commerce.productName());
+// console.log(faker.commerce.productName()); 
+// console.log(faker.commerce.productDescription());
+
+// console.log(faker.random.uuid()); 
+// console.log(faker.image.imageUrl());
+// console.log(faker.name.findName());
+
+const randomCategoryList =(n)=>{
+  if(n<=0) return [];
+
+  const categoryList=[]
+
+  Array.from(new Array(n)).forEach(()=>
+  {
+    const category ={
+      id : faker.random.uuid(),
+      name: faker.commerce.department(),
+      createdAt:Date.now(),
+      updatedAt:Date.now(),
+    }
+
+    categoryList.push(category);
+  })
+
+  return categoryList
+}
+
+const randomProductList = (categoryList, numberOfProducts) => {
+  if (numberOfProducts <= 0) return [];
+
+  const productList =[];
+  for(const category of categoryList) {
+      Array.from(new Array(numberOfProducts)).forEach(()=>{
+        const product = {
+          categoryId:category.id,
+          id :faker.random.uuid(),
+          name:faker.commerce.productName(),
+          price:Number.parseFloat(faker.commerce.price()),
+          description:faker.commerce.productDescription(),
+          createdAt:Date.now(),
+          updatedAt:Date.now(),
+          thumbnailUrl:faker.image.imageUrl(200,200),
+        }
+        productList.push(product);
+      })
+  }
+
+  return productList
+}
+
+
+// IFFE
+
+(()=>{
+  //random data
+
+  const categoryList =randomCategoryList(10);
+  const productList=randomProductList(categoryList,10);
+
+  //prepare db object
+  const db={
+    categories:categoryList,
+    products:productList,
+    profile:{
+      name:"Po",
+    }
+  };
+  //write db object to db.json
+  fs.writeFile('db.json',JSON.stringify(db),()=>{
+    console.log('generate successfully : ')
+  })
+
+})();
